e2e5cde9b576432478de9665bac775bc132d62da
ediblerope <fredrik@nordhammer.it>
2025-12-02 20:35:16 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,25 +1,18 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, ... }:
-
-let
-	hostname = "nixos-gaming";
-in
+{ config, pkgs, lib, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./modules/base.nix
-      (./modules + "/${hostname}.nix")  # Uses hostname from environment
-      ./modules/kde.nix
-      ./modules/krisp-patcher.nix
-      #<home-manager/nixos>
-    ];
-	networking.hostName = "${hostname}";
+imports = [
+	./hardware-configuration.nix
+	./base.nix
+];
 
-#######################################################
-system.stateVersion = "25.05";
+# Bootloader.
+boot.loader.grub.enable = true;
+boot.loader.grub.device = "/dev/vda";
+boot.loader.grub.useOSProber = true;
+
+networking.hostName = "FredOS-gaming";
+
+system.stateVersion = "25.11";
+
 }
