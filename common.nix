5720df080b3b86fadf2fadf9b0ca3bcb1a453a8b
ediblerope <fredrik@nordhammer.it>
2025-12-03 15:48:10 +0000

    Update common.nix

--- common.nix
+++ common.nix
@@ -2,6 +2,14 @@
 { config, pkgs, lib, ... }:
 
 {
+
+imports = [
+    ./common.nix
+    ./hosts/FredOS-Gaming.nix
+    ./hosts/FredOS-Macbook.nix
+    # Add all your hosts here
+  ];
+
 # Use latest kernel
 boot.kernelPackages = pkgs.linuxPackages_latest;
 
@@ -26,51 +34,6 @@ environment.systemPackages = with pkgs; [
 services.xserver.enable = true;
 services.displayManager.gdm.enable = true;
 services.desktopManager.gnome.enable = true;
-
-# Apply GNOME settings on login
-systemd.user.services.gnomeSettings = {
-  description = "Apply GNOME custom settings";
-  wantedBy = [ "default.target" ];
-  after = [
-    "graphical-session.target"
-    "gnome-session.target"
-    "gnome-settings-daemon.service"
-  ];
-  serviceConfig = {
-    Type = "oneshot";
-    ExecStart = pkgs.writeShellScript "apply-gnome-settings" ''
-      LOG=~/gnome-settings.log
-      echo "---- RUN $(date) ----" >> "$LOG"
-
-      export PATH=${pkgs.glib}/bin:$PATH
-      export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
-
-      # Ensure GNOME is fully ready
-      sleep 30
-      echo "Running settings..." >> "$LOG"
-
-      # Interface / theme
-      gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' >> "$LOG" 2>&1
-      gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' >> "$LOG" 2>&1
-      gsettings set org.gnome.desktop.interface accent-color 'purple' >> "$LOG" 2>&1
-
-	  # Media keys
-	  gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']" >> "$LOG" 2>&1
-	  gsettings set org.gnome.settings-daemon.plugins.media-keys control-center "['<Super>i']" >> "$LOG" 2>&1
-	
-      # Custom keybinding: Super+T → kgx
-	  gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']" >> "$LOG" 2>&1
-	  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal' >> "$LOG" 2>&1
-	  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'kgx' >> "$LOG" 2>&1
-	  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>t' >> "$LOG" 2>&1
-
-	  # Mouse acceleration
-	  gsettings set org.gnome.desktop.peripherals.mouse accel-profile "flat" >> "$LOG" 2>&1
-
-      echo "DONE" >> "$LOG"
-    '';
-  };
-};
 
 
 # Define a user account. Don't forget to set a password with ‘passwd’.
