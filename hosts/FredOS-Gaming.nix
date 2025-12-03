7e1689f84c8adadfac600ab726e756be55383399
ediblerope <fredrik@nordhammer.it>
2025-12-03 12:34:54 +0000

    Update FredOS-Gaming.nix

--- hosts/FredOS-Gaming.nix
+++ hosts/FredOS-Gaming.nix
@@ -7,6 +7,14 @@ config = lib.mkIf (config.networking.hostName == "FredOS-Gaming") {
 	# $ nix search wget
 	environment.systemPackages = with pkgs; [
 		#Package name
+		lutris
 	];
+	programs.steam = {
+  		enable = true;
+  		remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  		dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  		localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+	};
+	
 };
 }
