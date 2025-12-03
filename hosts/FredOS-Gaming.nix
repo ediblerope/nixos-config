cd9c21ab5212252c2544d5e39f3f6275baabba91
ediblerope <fredrik@nordhammer.it>
2025-12-03 14:56:34 +0000

    Update FredOS-Gaming.nix

--- hosts/FredOS-Gaming.nix
+++ hosts/FredOS-Gaming.nix
@@ -17,7 +17,17 @@ config = lib.mkIf (config.networking.hostName == "FredOS-Gaming") {
           pkgs: with pkgs; [
             kdePackages.breeze
           ];
+		};
 	};
+	system.autoUpgrade = {
+	  enable = true;
+	  dates = "daily";
+	  persistent = true;  # Run on next boot if the scheduled time was missed
+	  allowReboot = false;
+	  flags = [
+    "--upgrade"
+    "--option" "tarball-ttl" "0"
+  ];
 };
 	
 };
