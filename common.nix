1693e60f125602b6c24e53b890f861c16d086fef
ediblerope <fredrik@nordhammer.it>
2025-12-03 10:07:54 +0000

    Update common.nix

--- common.nix
+++ common.nix
@@ -5,30 +5,15 @@
 # Use latest kernel
 boot.kernelPackages = pkgs.linuxPackages_latest;
 
+# Shell aliases
+environment.shellAliases = {
+  update = "sudo nixos-rebuild switch --option tarball-ttl 0";
+};
+
 # Add packages
 environment.systemPackages = [
 	pkgs.git
 ];
- 
-# Enable networking
-networking.networkmanager.enable = true;
- 
-# Set your time zone.
-time.timeZone = "Europe/London";
- 
-# Select internationalisation properties.
-i18n.defaultLocale = "en_GB.UTF-8";
-i18n.extraLocaleSettings = {
-	LC_ADDRESS = "en_GB.UTF-8";
-	LC_IDENTIFICATION = "en_GB.UTF-8";
-	LC_MEASUREMENT = "en_GB.UTF-8";
-	LC_MONETARY = "en_GB.UTF-8";
-	LC_NAME = "en_GB.UTF-8";
-	LC_NUMERIC = "en_GB.UTF-8";
-	LC_PAPER = "en_GB.UTF-8";
-	LC_TELEPHONE = "en_GB.UTF-8";
-	LC_TIME = "en_GB.UTF-8";
-};
  
 # GNOME + Keybinds
 # Enable the X11 windowing system.
@@ -53,25 +38,6 @@ programs.dconf = {
 		};
 	}];
 };
- 
-# Configure keymap in X11
-services.xserver.xkb = {
-	layout = "gb";
-	variant = "";
-};
- 
-# Configure console keymap
-console.keyMap = "uk";
- 
-# Enable sound with pipewire.
-services.pulseaudio.enable = false;
-security.rtkit.enable = true;
-services.pipewire = {
-	enable = true;
-	alsa.enable = true;
-	alsa.support32Bit = true;
-	pulse.enable = true;
-};
 
 # Define a user account. Don't forget to set a password with ‘passwd’.
 users.users.fred = {
@@ -90,4 +56,47 @@ nixpkgs.config.allowUnfree = true;
  
 # Services
 services.flatpak.enable = true;
+
+######################
+##BORING STUFF BELOW##
+######################
+# Enable networking
+networking.networkmanager.enable = true;
+ 
+# Set your time zone.
+time.timeZone = "Europe/London";
+ 
+# Select internationalisation properties.
+i18n.defaultLocale = "en_GB.UTF-8";
+i18n.extraLocaleSettings = {
+	LC_ADDRESS = "en_GB.UTF-8";
+	LC_IDENTIFICATION = "en_GB.UTF-8";
+	LC_MEASUREMENT = "en_GB.UTF-8";
+	LC_MONETARY = "en_GB.UTF-8";
+	LC_NAME = "en_GB.UTF-8";
+	LC_NUMERIC = "en_GB.UTF-8";
+	LC_PAPER = "en_GB.UTF-8";
+	LC_TELEPHONE = "en_GB.UTF-8";
+	LC_TIME = "en_GB.UTF-8";
+};
+
+# Enable sound with pipewire.
+services.pulseaudio.enable = false;
+security.rtkit.enable = true;
+services.pipewire = {
+	enable = true;
+	alsa.enable = true;
+	alsa.support32Bit = true;
+	pulse.enable = true;
+};
+
+# Configure keymap in X11
+services.xserver.xkb = {
+	layout = "gb";
+	variant = "";
+};
+ 
+# Configure console keymap
+console.keyMap = "uk";
+
 }
