5c7b37b939fd15e6b90467c951bbf4ff2cb35102
ediblerope <fredrik@nordhammer.it>
2025-12-04 11:50:29 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
   description = "Fred's NixOS Flake";
 
   inputs = {
-    # 1. NixOS unstable (Replaces your 'nixos-unstable' channel)
+    # NixOS unstable
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # 2. Home Manager master (Replaces your 'home-manager master' channel)
+    # Home Manager master
     home-manager = {
       url = "github:nix-community/home-manager/master";
       # Ensure Home Manager uses the Nixpkgs input defined above
@@ -15,19 +15,10 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: {
-    # Define your single NixOS configuration
     nixosConfigurations.FredOS-Gaming = nixpkgs.lib.nixosSystem {
-      # Use the unstable system
       system = "x86_64-linux";
-      
-      # Import your existing configurations
       modules = [
-        # This is where you point to your existing common.nix
         ./common.nix 
-        
-        # 🌟 NEW: Home Manager module inclusion 🌟
-        # You declare HM as a module here instead of using the imports in common.nix
-        # NOTE: You MUST remove the imports block for Home Manager from common.nix!
         home-manager.nixosModules.home-manager
       ];
     };
