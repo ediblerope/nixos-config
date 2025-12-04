fc62e96d809c9ffe39bd7e82567bcdacb2695407
ediblerope <fredrik@nordhammer.it>
2025-12-04 12:41:17 +0000

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -12,37 +12,44 @@
   ];
 
   # --- GNOME/dconf Settings via Home Manager ---
-  # This section replaces the troublesome 'systemd.user.services.gnomeSettings' script!
-  dconf.settings = {
-    # Interface / theme (Example from your script)
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-      gtk-theme = "Adwaita-dark";
-      # accent-color is not directly exposed via dconf in this way
-    };
-
-    # Custom keybindings (Example from your script)
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      home = ["<Super>e"];
-      control-center = ["<Super>i"];
-      custom-keybindings = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-      ];
-    };
-
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-      name = "Terminal";
-      command = "kgx";
-      binding = "<Super>t";
-    };
-
-    # Mouse acceleration (Example from your script)
-    "org/gnome/desktop/peripherals/mouse" = {
-      accel-profile = "flat";
-    };
-
-    # You can add many more settings here, like wallpaper, etc.
+dconf.settings = {
+  # Interface / theme
+  "org/gnome/desktop/interface" = {
+    color-scheme = "prefer-dark";
+    gtk-theme = "Adwaita-dark";
   };
+
+  # Window manager keybindings
+  "org/gnome/desktop/wm/keybindings" = {
+    close = ["<Super>q"];
+    toggle-fullscreen = ["<Super>f"];
+  };
+
+  # Shell keybindings
+  "org/gnome/shell/keybindings" = {
+    show-screenshot-ui = ["<Shift><Super>s"];
+  };
+
+  # Custom keybindings
+  "org/gnome/settings-daemon/plugins/media-keys" = {
+    home = ["<Super>e"];
+    control-center = ["<Super>i"];
+    custom-keybindings = [
+      "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+    ];
+  };
+  
+  "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+    name = "Terminal";
+    command = "kgx";
+    binding = "<Super>t";
+  };
+
+  # Mouse acceleration
+  "org/gnome/desktop/peripherals/mouse" = {
+    accel-profile = "flat";
+  };
+};
 
   # --- Aliases (Moved from system-wide common.nix) ---
   # These aliases are now defined for the user's shell (e.g., bash/zsh).
