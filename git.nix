05604277535f6299c7f5ab890bbffdea5182e322
ediblerope <fredrik@nordhammer.it>
2025-12-04 12:12:36 +0000

    Update git.nix

--- git.nix
+++ git.nix
@@ -1,11 +1,18 @@
-# git.nix
 { config, pkgs, lib, ... }:
-
+let
+  home-manager = builtins.fetchTarball {
+    url = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  };
+in
 {
   imports = [
-    #./flake.nix
+    (import "${home-manager}/nixos")
     ./hosts/FredOS-Gaming.nix
     ./hosts/FredOS-Macbook.nix
-    # Add all your hosts here
+    ./common.nix
   ];
+  
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.fred = import ./home.nix;
 }
