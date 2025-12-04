ede453262473e29acebeb4588569087e0272d8ea
ediblerope <fredrik@nordhammer.it>
2025-12-04 13:31:58 +0000

    Update gnome.nix

--- settings/gnome.nix
+++ settings/gnome.nix
@@ -2,20 +2,20 @@
 { config, pkgs, lib, ... }:
 
 {
-
-# Enable Gnome
-# Enable the X11 windowing system.
-services.xserver.enable = true;
-services.displayManager.gdm.enable = true;
-services.desktopManager.gnome.enable = true;
-
-# Add extensions
-# Add packages
-environment.systemPackages = with pkgs; [
-	gnomeExtensions.blur-my-shell
-	gnomeExtensions.just-perfection
-	gnomeExtensions.appindicator
-	gnomeExtensions.hot-edge
-];
-
+config = lib.mkIf (config.networking.hostName == "FredOS-Gaming" || config.networking.hostName == "FredOS-Macbook") {
+	# Enable Gnome
+	# Enable the X11 windowing system.
+	services.xserver.enable = true;
+	services.displayManager.gdm.enable = true;
+	services.desktopManager.gnome.enable = true;
+	
+	# Add extensions
+	# Add packages
+	environment.systemPackages = with pkgs; [
+		gnomeExtensions.blur-my-shell
+		gnomeExtensions.just-perfection
+		gnomeExtensions.appindicator
+		gnomeExtensions.hot-edge
+	];
+};
 }
