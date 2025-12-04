a18a49ec80cd2d64128a28f09c55b5f314ee2f49
ediblerope <fredrik@nordhammer.it>
2025-12-04 13:19:11 +0000

    Update FredOS-Gaming.nix

--- hosts/FredOS-Gaming.nix
+++ hosts/FredOS-Gaming.nix
@@ -1,34 +1,37 @@
 { config, pkgs, lib, ... }:
-
 {
-config = lib.mkIf (config.networking.hostName == "FredOS-Gaming") {
-	
-	# List packages installed in system profile. To search, run:
-	# $ nix search wget
-	environment.systemPackages = with pkgs; [
-		#Package name
-		lutris
-	];
-	programs.steam = {
-	enable = true;
-	remotePlay.openFirewall = true;
-	package = pkgs.steam.override {
+  config = lib.mkIf (config.networking.hostName == "FredOS-Gaming") {
+    imports = [
+      ../settings/gnome.nix
+    ];
+    
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      #Package name
+      lutris
+    ];
+    
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      package = pkgs.steam.override {
         extraPkgs =
           pkgs: with pkgs; [
             kdePackages.breeze
           ];
-		};
-	};
-	system.autoUpgrade = {
-	  enable = true;
-	  dates = "daily";
-	  persistent = true;  # Run on next boot if the scheduled time was missed
-	  allowReboot = false;
-	  flags = [
-    "--upgrade"
-    "--option" "tarball-ttl" "0"
-  ];
-};
-	
-};
+      };
+    };
+    
+    system.autoUpgrade = {
+      enable = true;
+      dates = "daily";
+      persistent = true;  # Run on next boot if the scheduled time was missed
+      allowReboot = false;
+      flags = [
+        "--upgrade"
+        "--option" "tarball-ttl" "0"
+      ];
+    };
+  };
 }
