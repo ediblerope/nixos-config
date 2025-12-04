0923d15e26b58c577e2fb62bdfc59809da628374
ediblerope <fredrik@nordhammer.it>
2025-12-04 13:32:37 +0000

    Update FredOS-Macbook.nix

--- hosts/FredOS-Macbook.nix
+++ hosts/FredOS-Macbook.nix
@@ -1,36 +1,32 @@
 # hosts/FredOS-Macbook.nix
 { config, pkgs, lib, ... }:
 {
-  imports = lib.optionals (config.networking.hostName == "FredOS-Macbook") [
-    ../settings/gnome.nix
+config = lib.mkIf (config.networking.hostName == "FredOS-Macbook") {
+  environment.systemPackages = with pkgs; [
+    # Package names here
   ];
   
-  config = lib.mkIf (config.networking.hostName == "FredOS-Macbook") {
-    environment.systemPackages = with pkgs; [
-      # Package names here
-    ];
-    
-    # Bootloader
-    boot = {
-      loader = {
-        systemd-boot.enable = true;
-        efi.canTouchEfiVariables = true;
-      };
-      
-      # Enable Broadcom WL for Macbook
-      extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
-      
-      blacklistedKernelModules = [
-        "b43"
-        "bcma"
-        "ssb"
-      ];
+  # Bootloader
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
     };
     
-    hardware.enableRedistributableFirmware = true;
+    # Enable Broadcom WL for Macbook
+    extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
     
-    nixpkgs.config.permittedInsecurePackages = [
-      "broadcom-sta-6.30.223.271-59-6.17.9"
+    blacklistedKernelModules = [
+      "b43"
+      "bcma"
+      "ssb"
     ];
   };
+  
+  hardware.enableRedistributableFirmware = true;
+  
+  nixpkgs.config.permittedInsecurePackages = [
+    "broadcom-sta-6.30.223.271-59-6.17.9"
+  ];
+};
 }
