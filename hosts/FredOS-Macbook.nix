3991f623f3dea4da61109351d0ed5179d4a13f36
ediblerope <fredrik@nordhammer.it>
2025-12-04 18:13:01 +0000

    Update FredOS-Macbook.nix

--- hosts/FredOS-Macbook.nix
+++ hosts/FredOS-Macbook.nix
@@ -1,11 +1,6 @@
 # hosts/FredOS-Macbook.nix
 { config, pkgs, lib, ... }:
 {
-  # Move this OUTSIDE the mkIf
-  nixpkgs.config.permittedInsecurePackages = [
-    "broadcom-sta-6.30.223.271-59-6.18"
-  ];
-
   config = lib.mkIf (config.networking.hostName == "FredOS-Macbook") {
     environment.systemPackages = with pkgs; [
       # Package names here
@@ -19,7 +14,6 @@
       };
       
       # Enable Broadcom WL for Macbook
-      # Use config.boot.kernelPackages to match the kernel version from common.nix
       extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
       
       blacklistedKernelModules = [
@@ -30,5 +24,10 @@
     };
     
     hardware.enableRedistributableFirmware = true;
+    
+    # Put nixpkgs config INSIDE the mkIf
+    nixpkgs.config.permittedInsecurePackages = [
+      "broadcom-sta-6.30.223.271-59-6.18"
+    ];
   };
 }
