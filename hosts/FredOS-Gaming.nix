b8792fddd26607bb611403e424990dee47d7ce8b
ediblerope <fredrik@nordhammer.it>
2025-12-09 16:14:43 +0000

    Update FredOS-Gaming.nix

--- hosts/FredOS-Gaming.nix
+++ hosts/FredOS-Gaming.nix
@@ -1,21 +1,11 @@
+# hosts/FredOS-Gaming.nix
 { config, pkgs, lib, ... }:
-(lib.mkIf (config.networking.hostName == "FredOS-Gaming") { 
-    
-    # Remove this entire packageOverrides section - it's not working
-    
-    environment.systemPackages = [
-      pkgs.lutris
-      pkgs.adwaita-icon-theme
-      pkgs.nix-index
-      pkgs.libdecor
-      pkgs.pkgsi686Linux.libdecor  # This is the correct way
+{
+  config = lib.mkIf (config.networking.hostName == "FredOS-Gaming") {
+    environment.systemPackages = with pkgs; [
+      lutris
     ];
     
-    hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
-    }; # <--- Semicolon required here
-
     programs.steam = {
       enable = true;
       remotePlay.openFirewall = true;
@@ -24,13 +14,8 @@
           kdePackages.breeze
         ];
       };
-    }; # <--- Semicolon required here
-
-    environment.sessionVariables = {
-      LIBDECOR_PLUGIN_DIR = "${pkgs.libdecor}/lib/libdecor/plugins-1";
-      GTK_PATH = "${pkgs.gtk3}/lib/gtk-3.0:${pkgs.gtk4}/lib/gtk-4.0";
-    }; # <--- Semicolon required here
-
+    };
+    
     system.autoUpgrade = {
       enable = true;
       dates = "daily";
@@ -41,5 +26,5 @@
         "--option" "tarball-ttl" "0"
       ];
     };
-    
-})
+  };
+}
