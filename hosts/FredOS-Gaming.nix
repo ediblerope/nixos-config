c8b03c0b2e8c5c53d06b17260ae357f02651ca29
ediblerope <fredrik@nordhammer.it>
2025-12-09 15:53:21 +0000

    Update FredOS-Gaming.nix

--- hosts/FredOS-Gaming.nix
+++ hosts/FredOS-Gaming.nix
@@ -2,51 +2,50 @@
 
 (lib.mkIf (config.networking.hostName == "FredOS-Gaming") { 
 
-    nixpkgs.config.packageOverrides = pkgs: {
-      pkgs = pkgs // {
-        lib32 = pkgs.pkgsi686Linux.pkgs;
-      };
-    }; # <--- ADD SEMICOLON HERE
-    
-    environment.systemPackages = with pkgs; [
-      lutris
-      adwaita-icon-theme
-      nix-index
-      libdecor
-      pkgs.lib32.libdecor
-    ]; # <--- ADD SEMICOLON HERE
+    nixpkgs.config.packageOverrides = pkgs: {
+      pkgs = pkgs // {
+        lib32 = pkgs.pkgsi686Linux.pkgs;
+      };
+    }; # <--- SEMICOLON (Required)
+    
+    environment.systemPackages = with pkgs; [
+      lutris
+      adwaita-icon-theme
+      nix-index
+      libdecor
+      pkgs.lib32.libdecor
+    ]; # <--- SEMICOLON (Required)
 
-    hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
-    }; # <--- ADD SEMICOLON HERE
-    
-    # ... continue adding semicolons to the end of every top-level option ...
-    
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    }; # <--- SEMICOLON (Required)
+
     programs.steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      package = pkgs.steam.override {
-        extraPkgs = pkgs: with pkgs; [
-          kdePackages.breeze
-        ];
-      };
-    }; # <--- ADD SEMICOLON HERE
+      enable = true;
+      remotePlay.openFirewall = true;
+      package = pkgs.steam.override {
+        extraPkgs = pkgs: with pkgs; [
+          kdePackages.breeze
+        ];
+      };
+    }; # <--- SEMICOLON (Required)
 
-    environment.sessionVariables = {
-      LIBDECOR_PLUGIN_DIR = "${pkgs.libdecor}/lib/libdecor/plugins-1";
-      GTK_PATH = "${pkgs.gtk3}/lib/gtk-3.0:${pkgs.gtk4}/lib/gtk-4.0";
-    }; # <--- ADD SEMICOLON HERE
+    environment.sessionVariables = {
+      LIBDECOR_PLUGIN_DIR = "${pkgs.libdecor}/lib/libdecor/plugins-1";
+      GTK_PATH = "${pkgs.gtk3}/lib/gtk-3.0:${pkgs.gtk4}/lib/gtk-4.0";
+    }; # <--- SEMICOLON (Required)
 
-    system.autoUpgrade = {
-      enable = true;
-      dates = "daily";
-      persistent = true;
-      allowReboot = false;
-      flags = [
-        "--upgrade"
-        "--option" "tarball-ttl" "0"
-      ];
-    }; # <--- ADD SEMICOLON HERE
-
+    system.autoUpgrade = {
+      enable = true;
+      dates = "daily";
+      persistent = true;
+      allowReboot = false;
+      flags = [
+        "--upgrade"
+        "--option" "tarball-ttl" "0"
+      ];
+    }; # <--- SEMICOLON (Required)
+    
+# Closing brace for the attribute set, closing parenthesis for lib.mkIf
 })
