fadec133a12672583ab1fccc6899d6ef816bda1f
ediblerope <fredrik@nordhammer.it>
2025-12-19 09:31:22 +0000

    Update flatpaks.nix

--- apps/flatpaks.nix
+++ apps/flatpaks.nix
@@ -1,8 +1,15 @@
-# flatpaks.nix
 { config, pkgs, ... }:
+
+let
+  # This fetches the code directly from GitHub without needing a 'channel'
+  nix-flatpak = builtins.fetchTarball {
+    url = "https://github.com/gmodena/nix-flatpak/archive/main.tar.gz";
+    # You can add a sha256 here for security later
+  };
+in
 {
   imports = [
-    <nix-flatpak/nixos.nix>
+    "${nix-flatpak}/nixos.nix"
   ];
   
   services.flatpak = {
