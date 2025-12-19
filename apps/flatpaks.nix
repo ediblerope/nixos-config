cd77163505b0b4dcb39f8ac4f74715fc6a82797d
ediblerope <fredrik@nordhammer.it>
2025-12-19 09:16:14 +0000

    Create flatpaks.nix

---
+++ apps/flatpaks.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    <nix-flatpak/nixos.nix>
+  ];
+  
+  services.flatpak = {
+    enable = true;
+    packages = [
+      "io.github.zen_browser.zen"
+    ];
+    overrides = {
+      "io.github.zen_browser.zen" = {
+        Context.filesystems = [ "home:rw" ];
+      };
+    };
+  };
+}
