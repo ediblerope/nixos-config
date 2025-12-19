5dc8be34ea66d13c7ba8206d77bdc1edbc538755
ediblerope <fredrik@nordhammer.it>
2025-12-19 11:34:34 +0000

    Update FredOS-Macbook.nix

--- hosts/FredOS-Macbook.nix
+++ hosts/FredOS-Macbook.nix
@@ -4,8 +4,11 @@
   config = lib.mkIf (config.networking.hostName == "FredOS-Macbook") {
     environment.systemPackages = with pkgs; [
       # Package names here
-      tlp
+      
     ];
+
+    # Enable tlp service
+    services.tlp.enable = true;
     
     # Bootloader
     boot = {
@@ -28,7 +31,7 @@
     
     # Put nixpkgs config INSIDE the mkIf
     nixpkgs.config.permittedInsecurePackages = [
-      "broadcom-sta-6.30.223.271-59-6.18.1"
+      pkgs.lib.getName config.boot.kernelPackages.broadcom_sta
     ];
   };
 }
