b377396b3188a8cc978da344287effffdd4ce434
ediblerope <fredrik@nordhammer.it>
2025-12-19 11:43:12 +0000

    Update FredOS-Macbook.nix

--- hosts/FredOS-Macbook.nix
+++ hosts/FredOS-Macbook.nix
@@ -4,9 +4,11 @@
   # Use mkMerge to allow the insecure package rule to sit alongside the mkIf block
   config = lib.mkMerge [
     # 1. This part always applies if this file is imported
-    {
+    {{
       nixpkgs.config.permittedInsecurePackages = [
-        (pkgs.lib.getName config.boot.kernelPackages.broadcom_sta)
+        # Reconstructs the name: broadcom-sta- + driver-version + - + kernel-version
+        # The driver version for Broadcom STA is currently "6.30.223.271"
+        "broadcom-sta-6.30.223.271-${config.boot.kernelPackages.kernel.version}"
       ];
     }
 
