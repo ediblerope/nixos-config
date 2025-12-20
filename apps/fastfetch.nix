522bfd4bc22abc7481565da279c6499542288301
ediblerope <fredrik@nordhammer.it>
2025-12-20 22:41:37 +0000

    Update fastfetch.nix

--- apps/fastfetch.nix
+++ apps/fastfetch.nix
@@ -25,9 +25,9 @@
       "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
       "logo": {
         "source": "/etc/fastfetch/logo.png",
-        "type": "kitty-direct",
-        "width": 30,
-        "height": 15,
+        "type": "auto",
+        "width": 25,
+        "height": 12,
         "padding": {
           "top": 1,
           "left": 2,
