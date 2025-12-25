b41f68d4f86a29fb7058c0b555cc6dcf6a619ed9
ediblerope <fredrik@nordhammer.it>
2025-12-25 12:02:23 +0000

    Update flatpaks.nix

--- apps/flatpaks.nix
+++ apps/flatpaks.nix
@@ -16,14 +16,14 @@
     
     packages = [
       "app.zen_browser.zen"
-      "dev.vencord.vesktop"
+      "ddev.vencord.Vesktop
     ];
     
     overrides = {
       "app.zen_browser.zen" = {
         Context.filesystems = [ "home:rw" ];
       };
-      "dev.vencord.vesktop" = {
+      "dev.vencord.Vesktop" = {
         Context.filesystems = [ "home:rw" ];
       };
     };
