6cbf9ef7ba27eece4970c556b6a0fdbb24c7508a
ediblerope <fredrik@nordhammer.it>
2025-12-25 11:17:35 +0000

    Update flatpaks.nix

--- apps/flatpaks.nix
+++ apps/flatpaks.nix
@@ -1,23 +1,24 @@
 { config, pkgs, ... }:
-
-let
-  nix-flatpak = builtins.fetchTarball {
-    url = "https://github.com/gmodena/nix-flatpak/archive/main.tar.gz";
-  };
-in
 {
   imports = [
-    # Pointing to the specific sub-folder path
-    "${nix-flatpak}/modules/nixos.nix"
+    <nix-flatpak/modules/nixos.nix>
   ];
   
   services.flatpak = {
     enable = true;
-    # This matches the options provided by the nix-flatpak module
+    
+    remotes = [
+      {
+        name = "flathub";
+        location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+      }
+    ];
+    
     packages = [
       "io.github.zen_browser.zen"
       "dev.vencord.Vesktop"
     ];
+    
     overrides = {
       "io.github.zen_browser.zen" = {
         Context.filesystems = [ "home:rw" ];
