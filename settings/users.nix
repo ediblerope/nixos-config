2e646d30546c19058e03fc0970dd96987379bd7b
ediblerope <fredrik@nordhammer.it>
2026-01-09 13:07:18 +0000

    Update users.nix

--- settings/users.nix
+++ settings/users.nix
@@ -2,14 +2,16 @@
 { config, pkgs, lib, ... }:
 
 {
-# Define a user account. Don't forget to set a password with ‘passwd’.
-users.users.fred = {
-    isNormalUser = true;
-    description = "fred";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      bazaar
-      fastfetch
-    ];
-  };
+	config = lib.mkIf (lib.elem config.networking.hostName [ "FredOS-Gaming" "FredOS-Macbook" ]) {
+	    # Define a user account. Don't forget to set a password with 'passwd'.
+		users.users.fred = {
+			isNormalUser = true;
+			description = "fred";
+			extraGroups = [ "networkmanager" "wheel" ];
+			packages = with pkgs; [
+				bazaar
+				fastfetch
+			];
+		};
+	};
 }
