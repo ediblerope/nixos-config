a732424c62e3cc93105d865d3855aa9a3d2c3dd1
ediblerope <fredrik@nordhammer.it>
2026-01-10 20:30:28 +0000

    Update audio.nix

--- settings/audio.nix
+++ settings/audio.nix
@@ -1,14 +1,14 @@
 # audio.nix
 { config, pkgs, lib, ... }:
+
 {
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
+# Enable sound with pipewire.
+services.pulseaudio.enable = false;
+security.rtkit.enable = true;
+services.pipewire = {
+	enable = true;
+	alsa.enable = true;
+	alsa.support32Bit = true;
+	pulse.enable = true;
+};
 }
