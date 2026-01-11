7fa1480866e5b6fddae781e8af1521ae9f9e08a6
ediblerope <fredrik@nordhammer.it>
2026-01-11 10:24:38 +0000

    Update FredOS-Gaming.nix

--- hosts/FredOS-Gaming.nix
+++ hosts/FredOS-Gaming.nix
@@ -39,7 +39,6 @@
     # Bootloader
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.timeout = 1;
     
     system.autoUpgrade = {
       enable = true;
