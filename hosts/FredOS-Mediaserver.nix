37454f0b7e24178faa326428f64c572e50e4cf8e
ediblerope <fredrik@nordhammer.it>
2026-01-20 12:43:18 +0000

    Update FredOS-Mediaserver.nix

--- hosts/FredOS-Mediaserver.nix
+++ hosts/FredOS-Mediaserver.nix
@@ -1,40 +1,40 @@
 { config, pkgs, lib, ... }:
 {
-config = lib.mkIf (config.networking.hostName == "FredOS-Mediaserver") {
-  fileSystems."/mnt/disk1" = {
-    device = "/dev/disk/by-uuid/90ae3493-38c1-4473-b409-e9d99c3b315e";
-    fsType = "ext4";
-    options = [ "defaults" ];
-  };
-  fileSystems."/mnt/disk2" = {
-    device = "/dev/disk/by-uuid/7145223e-f285-424a-a114-cb0b1b64e068";
-    fsType = "ext4";
-    options = [ "defaults" ];
-  };
-  fileSystems."/mnt/disk3" = {
-    device = "/dev/disk/by-uuid/613abe28-9af9-481e-9673-0eab1913b32c";
-    fsType = "ext4";
-    options = [ "defaults" ];
-  };
-  fileSystems."/mnt/storage" = {
-    device = "/mnt/disk1:/mnt/disk2:/mnt/disk3";
-    fsType = "fuse.mergerfs";
-    options = [
-      "defaults"
-      "allow_other"
-      "use_ino"
-      "cache.files=partial"
-      "dropcacheonclose=true"
-      "category.create=mfs"  # Most free space for new files
-    ];
-  };
+#config = lib.mkIf (config.networking.hostName == "FredOS-Mediaserver") {
+#  fileSystems."/mnt/disk1" = {
+#    device = "/dev/disk/by-uuid/90ae3493-38c1-4473-b409-e9d99c3b315e";
+#    fsType = "ext4";
+#    options = [ "defaults" ];
+#  };
+#  fileSystems."/mnt/disk2" = {
+#    device = "/dev/disk/by-uuid/7145223e-f285-424a-a114-cb0b1b64e068";
+#    fsType = "ext4";
+#    options = [ "defaults" ];
+#  };
+#  fileSystems."/mnt/disk3" = {
+#    device = "/dev/disk/by-uuid/613abe28-9af9-481e-9673-0eab1913b32c";
+#    fsType = "ext4";
+#    options = [ "defaults" ];
+#  };
+#  fileSystems."/mnt/storage" = {
+#    device = "/mnt/disk1:/mnt/disk2:/mnt/disk3";
+#    fsType = "fuse.mergerfs";
+#    options = [
+#      "defaults"
+#      "allow_other"
+#      "use_ino"
+#      "cache.files=partial"
+#      "dropcacheonclose=true"
+#      "category.create=mfs"  # Most free space for new files
+#    ];
+#  };
 
   # Create symlink from home to storage
-  systemd.tmpfiles.rules = [
-    "L+ /home/fred/storage - - - - /mnt/storage"
-    "d /var/lib/nginx-proxy-manager/data 0755 root root -"
-    "d /var/lib/nginx-proxy-manager/letsencrypt 0755 root root -"
-  ];
+#  systemd.tmpfiles.rules = [
+#    "L+ /home/fred/storage - - - - /mnt/storage"
+#    "d /var/lib/nginx-proxy-manager/data 0755 root root -"
+#    "d /var/lib/nginx-proxy-manager/letsencrypt 0755 root root -"
+#  ];
 
   # Basic system packages
   environment.systemPackages = with pkgs; [
