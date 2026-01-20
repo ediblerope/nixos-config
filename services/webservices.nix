41fbb8db131f022a90541e987c33244fab3502ff
ediblerope <fredrik@nordhammer.it>
2026-01-20 21:35:57 +0000

    Update webservices.nix

--- services/webservices.nix
+++ services/webservices.nix
@@ -2,9 +2,40 @@
 {
 	config = lib.mkIf (config.networking.hostName == "FredOS-Mediaserver") {
 
+	# Nginx Proxy Manager
+	virtualisation.oci-containers = {
+      backend = "docker";
+      
+      containers."nginx-proxy-manager" = {
+        image = "jc21/nginx-proxy-manager:latest";
+        ports = [ 
+          "80:80"
+          "81:81"
+          "443:443"
+        ];
+        volumes = [
+          "/var/lib/nginx-proxy-manager/data:/data"
+          "/var/lib/nginx-proxy-manager/letsencrypt:/etc/letsencrypt"
+        ];
+        extraOptions = [
+          "--restart=unless-stopped"
+        ];
+      };
+    };
+    
+    # Create directories
+    systemd.tmpfiles.rules = [
+      "d /var/lib/nginx-proxy-manager/data 0755 root root -"
+      "d /var/lib/nginx-proxy-manager/letsencrypt 0755 root root -"
+    ];
+    
+    # Open firewall
+    networking.firewall.allowedTCPPorts = [ 80 81 443 ];
+
 	# Jellyfin
     services.jellyfin = {
       enable = true;
       openFirewall = true;
     };
+
 }
