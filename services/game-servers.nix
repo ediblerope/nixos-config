83666e7e7dcab4bed3a52b2ec8cfdf0e80880ccc
ediblerope <fredrik@nordhammer.it>
2026-01-20 21:06:31 +0000

    Update game-servers.nix

--- services/game-servers.nix
+++ services/game-servers.nix
@@ -1,28 +1,29 @@
 { config, pkgs, lib, ... }:
 {
-
-## <----- HYTALE ----> ##
-	virtualisation.oci-containers = {
-		backend = "docker";
-		containers."hytale-server" = {
-			image = "ghcr.io/terkea/hytale-server:latest";
-			ports = [ "5520:5520/udp" ];
-			environment = {
-				SERVER_NAME = "Nordhammer.it Hytale Server";
-				MAX_PLAYERS = "50";
-				MEMORY = "4G";
-				ENABLE_BACKUP = "true";
-				BACKUP_FREQUENCY = "30";
-				PASSWORD = "DukeSmells";
+	config = lib.mkIf (config.networking.hostName == "FredOS-Mediaserver") {
+	## <----- HYTALE ----> ##
+		virtualisation.oci-containers = {
+			backend = "docker";
+			containers."hytale-server" = {
+				image = "ghcr.io/terkea/hytale-server:latest";
+				ports = [ "5520:5520/udp" ];
+				environment = {
+					SERVER_NAME = "Nordhammer.it Hytale Server";
+					MAX_PLAYERS = "50";
+					MEMORY = "4G";
+					ENABLE_BACKUP = "true";
+					BACKUP_FREQUENCY = "30";
+					PASSWORD = "DukeSmells";
+				};
+				volumes = [
+					"/home/fred/docker/hytale-server/hytale-data:/data"
+				];
+				extraOptions = [
+					"--interactive=false"
+					"--tty=false"
+				];
 			};
-			volumes = [
-				"/home/fred/docker/hytale-server/hytale-data:/data"
-			];
-			extraOptions = [
-				"--interactive=false"
-				"--tty=false"
-			];
 		};
+		networking.firewall.allowedUDPPorts = [ 5520 ];
 	};
-	networking.firewall.allowedUDPPorts = [ 5520 ];
 }
