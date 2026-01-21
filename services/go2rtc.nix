2007cfa7ac8f5a458779eaade763df83a911326b
ediblerope <fredrik@nordhammer.it>
2026-01-21 11:45:07 +0000

    Update go2rtc.nix

--- services/go2rtc.nix
+++ services/go2rtc.nix
@@ -4,28 +4,27 @@
 	config = lib.mkIf (config.networking.hostName == "FredOS-Mediaserver") {
 
 		virtualisation.oci-containers = {
-		  backend = "docker";
-		
-		  # Authelia
-		  containers."authelia" = {
-			image = "authelia/authelia:latest";
-			volumes = [
-			  "/home/fred/docker/authelia/config.yml:/config/config.yml:ro"
-			  "/home/fred/docker/authelia/secrets:/secrets:ro"
-			];
-			ports = [ "9091:9091" ];
-			extraOptions = "--restart unless-stopped";
-		  };
-		
-		  # Go2RTC
-		  containers."go2rtc" = {
-			image = "blakeblackshear/go2rtc:latest";
-			volumes = [
-			  "/home/fred/docker/go2rtc/config.yml:/config/config.yml:ro"
-			];
-			ports = [ "1984:1984" ];
-			extraOptions = "--restart unless-stopped";
-		  };
+			backend = "docker";
+
+			# Authelia
+			containers."authelia" = {
+			  image = "authelia/authelia:latest";
+			  volumes = [
+				"/home/fred/docker/authelia/config.yml:/config/config.yml:ro"
+				"/home/fred/docker/authelia/secrets:/secrets:ro"
+			  ];
+			  ports = [ "9091:9091" ];
+			  extraOptions = [ "--restart" "unless-stopped" ];
+			};
+			
+			containers."go2rtc" = {
+			  image = "blakeblackshear/go2rtc:latest";
+			  volumes = [
+				"/home/fred/docker/go2rtc/config.yml:/config/config.yml:ro"
+			  ];
+			  ports = [ "1984:1984" ];
+			  extraOptions = [ "--restart" "unless-stopped" ];
+			};
 		};
 		# Create directories for local secrets
 		systemd.tmpfiles.rules = [
