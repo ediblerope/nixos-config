732da5d1599e7272dd5e17d3651f2d6c38f3e9b0
ediblerope <fredrik@nordhammer.it>
2026-01-21 09:28:50 +0000

    Update go2rtc.nix

--- services/go2rtc.nix
+++ services/go2rtc.nix
@@ -1,6 +1,18 @@
 { config, pkgs, lib, ... }:
 {
 	config = lib.mkIf (config.networking.hostName == "FredOS-Mediaserver") {
-
+		inputs = {
+		    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		    sops-nix.url = "github:Mic92/sops-nix";
+		  };
+		
+		  outputs = { self, nixpkgs, sops-nix }: {
+		    nixosConfigurations.yourhostname = nixpkgs.lib.nixosSystem {
+		      modules = [
+		        sops-nix.nixosModules.sops
+		        ./configuration.nix
+		      ];
+		    };
+		};
 	};
 }
