a08178ec422712db194328c6baae74839dd48a76
ediblerope <fredrik@nordhammer.it>
2026-01-22 10:48:10 +0000

    Update prowlarr.nix

--- services/prowlarr.nix
+++ services/prowlarr.nix
@@ -9,18 +9,6 @@
       openFirewall = true;
       dataDir = "/var/lib/prowlarr";
       user = "prowlarr";
-      group = "media";
     };
-
-    # Media group is already created in qbittorrent-nox.nix
-    # Just make sure sonarr is in it
-    users.users.prowlarr = {
-      isSystemUser = true;
-      group = "media";
-      extraGroups = [ "media" ];
-    };
-    
-    # Also add jellyfin to media group for reading
-    users.users.jellyfin.extraGroups = [ "media" ];
   };
 }
