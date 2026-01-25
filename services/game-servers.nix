526648b95299d262ebced9d09f5a8af84d90db3b
ediblerope <fredrik@nordhammer.it>
2026-01-25 15:53:44 +0000

    Update game-servers.nix

--- services/game-servers.nix
+++ services/game-servers.nix
@@ -2,28 +2,6 @@
 {
   config = lib.mkIf (config.networking.hostName == "FredOS-Mediaserver") {
     ## <----- HYTALE ----> ##
-    virtualisation.oci-containers = {
-      backend = "docker";
-      containers."hytale" = {
-        image = "indifferentbroccoli/hytale-server-docker:latest";
-        ports = [ "5520:5520/udp" ];
-        environment = {
-          SERVER_NAME = "Nordhammer.it Hytale Server";
-          MAX_PLAYERS = "50";
-          ENABLE_BACKUP = "true";
-          BACKUP_FREQUENCY = "30";
-          PASSWORD = "DukeSmells";
-        };
-        volumes = [
-          "/home/fred/docker/hytale-server/hytale-data:/home/hytale/server-files"
-        ];
-        extraOptions = [
-          "--stop-timeout=30"
-          "--memory=16g"  # Docker memory limit
-          "--memory-swap=16g"  # Prevent swap usage
-        ];
-      };
-    };
-    networking.firewall.allowedUDPPorts = [ 5520 ];
+    #
   };
 }
