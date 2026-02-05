d91f0a8ad6290ae254f020171813e1bca68425a1
ediblerope <fredrik@nordhammer.it>
2026-02-05 12:14:27 +0000

    Update locale.nix

--- settings/locale.nix
+++ settings/locale.nix
@@ -3,7 +3,8 @@
 
 {
 # Set your time zone.
-time.timeZone = "Europe/London";
+#time.timeZone = "Europe/London";
+services.automatic-timezoned.enable = true;
  
 # Select internationalisation properties.
 i18n.defaultLocale = "en_GB.UTF-8";
