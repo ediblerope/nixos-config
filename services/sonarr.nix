9ea2f264e952d3a9f43f20044323e4231d3055f0
ediblerope <fredrik@nordhammer.it>
2026-02-20 21:43:19 +0000

    Update sonarr.nix

--- services/sonarr.nix
+++ services/sonarr.nix
@@ -19,17 +19,5 @@
       group = "media";
       extraGroups = [ "media" ];
     };
-    
-    # Set up directory structure with proper permissions
-    systemd.tmpfiles.rules = [
-      # Downloads folder - qbittorrent writes here (already in qbittorrent-nox.nix)
-      "Z /mnt/storage/torrents/downloads 0775 qbittorrent media -"
-      
-      # Media folders - sonarr writes here
-      "d /mnt/storage/torrents/shows 0775 sonarr media -"
-      "Z /mnt/storage/torrents/shows 0775 sonarr media -"
-      "d /mnt/storage/torrents/audiobooks 0775 sonarr media -"
-      "Z /mnt/storage/torrents/audiobooks 0775 sonarr media -"
-    ];
   };
 }
