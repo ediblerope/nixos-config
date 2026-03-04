cb5853509d3fc621187f4f24cc8cff44ce34689f
ediblerope <fredrik@nordhammer.it>
2026-03-04 14:59:20 +0000

    Update audio.nix

--- settings/audio.nix
+++ settings/audio.nix
@@ -1,14 +1,21 @@
 # audio.nix
 { config, pkgs, lib, ... }:
-
 {
-# Enable sound with pipewire.
-services.pulseaudio.enable = false;
-security.rtkit.enable = true;
-services.pipewire = {
-	enable = true;
-	alsa.enable = true;
-	alsa.support32Bit = true;
-	pulse.enable = true;
-};
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  environment.etc."pipewire/pipewire-pulse.d/10-quirk-rules.conf".text = ''
+    pulse.rules = [
+      {
+        matches = [ { application.name = "~Chromium.*" } ]
+        actions = { quirks = [ block-source-volume ] }
+      }
+    ]
+  '';
 }
