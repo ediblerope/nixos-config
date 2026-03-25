fb4b626731555da661293026c2fcc70f77a85e57
ediblerope <fredrik@nordhammer.it>
2026-03-25 10:09:45 +0000

    Update gnome.nix

--- settings/gnome.nix
+++ settings/gnome.nix
@@ -38,6 +38,9 @@
           icon-theme = "Papirus";
           cursor-theme = "Adwaita";
         };
+        "org/gnome/mutter" = {
+          experimental-features = [ "variable-refresh-rate" ];
+        };
       };
     }];
     
