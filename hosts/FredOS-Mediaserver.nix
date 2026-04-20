9f0b2204272ec6bab5c3de056f35202e451b1335
ediblerope <fredrik@nordhammer.it>
2026-04-20 11:34:49 +0100

    Corrected sensors.

--- hosts/FredOS-Mediaserver.nix
+++ hosts/FredOS-Mediaserver.nix
@@ -19,7 +19,7 @@
       ghostty.terminfo
       matugen
       usbutils
-      lm-sensors
+      lm_sensors
       (pkgs.writeShellScriptBin "transcode-hevc" ''
         export PATH="${pkgs.jellyfin-ffmpeg}/bin:${pkgs.coreutils}/bin:${pkgs.findutils}/bin:${pkgs.gnugrep}/bin:${pkgs.gawk}/bin:${pkgs.bc}/bin:${pkgs.curl}/bin:$PATH"
         exec ${pkgs.bash}/bin/bash ${../scripts/transcode-hevc.sh} "$@"
